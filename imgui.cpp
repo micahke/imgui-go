d82aa0361f6acff5672840ce8ead961ab4e7f357
AllenDang <allengnr@gmail.com>
2021-08-30 21:21:28 +0800

    Update to imgui 1.84.2

--- imgui.cpp
+++ imgui.cpp
@@ -1,4 +1,4 @@
-// dear imgui, v1.84b
+// dear imgui, v1.84
 // (main code and documentation)
 
 // Help:
@@ -3937,7 +3937,6 @@ ImGuiKeyModFlags ImGui::GetMergedKeyModFlags()
     return key_mod_flags;
 }
 
-
 double ImGui::GetEventWaitingTime()
 {
     ImGuiContext& g = *GImGui;
@@ -6647,9 +6646,11 @@ void ImGui::BeginDisabled(bool disabled)
 {
     ImGuiContext& g = *GImGui;
     bool was_disabled = (g.CurrentItemFlags & ImGuiItemFlags_Disabled) != 0;
-    g.DisabledAlphaBackup = g.Style.Alpha;
     if (!was_disabled && disabled)
+    {
+        g.DisabledAlphaBackup = g.Style.Alpha;
         g.Style.Alpha *= g.Style.DisabledAlpha; // PushStyleVar(ImGuiStyleVar_Alpha, g.Style.Alpha * g.Style.DisabledAlpha);
+    }
     if (was_disabled || disabled)
         g.CurrentItemFlags |= ImGuiItemFlags_Disabled;
     g.ItemFlagsStack.push_back(g.CurrentItemFlags);
@@ -6661,7 +6662,7 @@ void ImGui::EndDisabled()
     bool was_disabled = (g.CurrentItemFlags & ImGuiItemFlags_Disabled) != 0;
     //PopItemFlag();
     g.ItemFlagsStack.pop_back();
-    g.CurrentItemFlags &= ~ImGuiItemFlags_Disabled;
+    g.CurrentItemFlags = g.ItemFlagsStack.back();
     if (was_disabled && (g.CurrentItemFlags & ImGuiItemFlags_Disabled) == 0)
         g.Style.Alpha = g.DisabledAlphaBackup; //PopStyleVar();
 }
