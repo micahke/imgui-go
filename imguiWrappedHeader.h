51f989724b52b5f84f0b3d3c444b946fdabcb46e
Allen Dang <allengnr@gmail.com>
2022-02-04 11:12:09 +0800

    Enable 32-bit vertex indices.

--- imguiWrappedHeader.h
+++ imguiWrappedHeader.h
@@ -3,4 +3,6 @@
 // This header is for use within the wrapper sources to ensure identical inclusion
 // of the core imgui header with all the various options.
 
+#define ImDrawIdx unsigned int
+
 #include "imgui.h"
